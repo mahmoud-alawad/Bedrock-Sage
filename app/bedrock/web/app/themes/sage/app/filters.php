cb2b5548ac9d416474afba1993042c7009ab4fd9
mahmoud <awad25.ma@gmail.com>
2022-07-03 15:08:40 +0200

    added classes filter to nav menu

--- app/bedrock/web/app/themes/sage/app/filters.php
+++ app/bedrock/web/app/themes/sage/app/filters.php
@@ -14,3 +14,16 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+/*
+ * return classes string to add on li items in nav menu
+ * */
+function add_additional_class_li( $classes, $item, $args)
+{
+    if (isset($args->li_class)) {
+        $classes[] = $args->li_class;
+    }
+    return $classes;
+}
+
+add_filter('nav_menu_css_class','add_additional_class_li',10,4);
